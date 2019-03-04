015dd242a575c6603cee10288a7907052c9051fb
rodriguez-facundo <bruseghini.f@gmail.com>
2019-03-04 09:46:48 -0300

    typo in copy

--- k8s/craft/copy.sh
+++ k8s/craft/copy.sh
@@ -1,5 +1,5 @@
 copy_files() {
   printf '\e[1;33m==>\e[37;1m COPYING FILES HERE !!!! \e[0m\n'
-  rm composer.json rm composer.lock
+  rm /var/www/html/composer.json /var/www/html/composer.lock
 	cp -r /tmp/html/. /var/www/html/
 }
