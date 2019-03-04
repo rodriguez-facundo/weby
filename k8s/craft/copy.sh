51d0f7e1c0e411764079beff7f5014b4771cbfdd
rodriguez-facundo <bruseghini.f@gmail.com>
2019-03-04 11:04:44 -0300

    coming from meta

--- k8s/craft/copy.sh
+++ k8s/craft/copy.sh
@@ -1,5 +1,5 @@
 copy_files() {
   printf '\e[1;33m==>\e[37;1m COPYING FILES HERE !!!! \e[0m\n'
-  rm /var/www/html/* /tmp/html/composer.base
 	cp -r /tmp/html/. /var/www/html/
+  rm /var/www/html/composer.base
 }
